a8df338cd0c19247bd717360bfea5c27116eed54
fearaujo293 <fearaujo487@gmail.com>
2025-04-17 17:06:35 -0300

    SecurityConfig.java

--- Treino_API-main/Treino/src/main/java/Security/SecurityConfig.java
+++ Treino_API-main/Treino/src/main/java/Security/SecurityConfig.java
@@ -1,20 +1,20 @@
 package Security;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
-        return http.build();
-    }
-}
+	 @Bean
+	    public OpenAPI customOpenAPI() {
+	        return new OpenAPI()
+	            .info(new Info()
+	                .title("Treino API")
+	                .version("1.0")
+	                .description("Documentação da API de Treino com Swagger"));
+	    }
+	}
